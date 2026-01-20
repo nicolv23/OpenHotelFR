641db3887d2cdc44a9f31b8e0a00c909d1d65b11
nicolv23 <nicolaslv0018@gmail.com>
2026-01-19 22:32:36 -0500

    chore: finalisation de la branche setup-flask (tests OK)

---
+++ tests/test_app.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from app import create_app
+
+def test_app_creation():
+    app = create_app()
+    assert app is not None
