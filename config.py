84a2a04ca099f464de263abae5609d058127b861
nicolv23 <nicolaslv0018@gmail.com>
2026-01-19 22:38:25 -0500

    feat: ajout de la configuration SQLAlchemy dans Config

--- config.py
+++ config.py
@@ -2,3 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "dev-secret-key"
+
+    # Configuration de la base de données
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///hotel.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
