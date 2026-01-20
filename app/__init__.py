32ad2d48402d5a82ff4f8dea50fbc0d7a9cdaae9
nicolv23 <nicolaslv0018@gmail.com>
2026-01-19 22:51:05 -0500

    feat: initialisation de la base MySQL au démarrage de l'application

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from .bd import db
+from .models import Chambre, Client, Reservation
+from .routes.main import main_bp
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object("config.Config")
 
-    # Import des routes
-    from app.routes.main import main_bp
+    # Connexion à la base
+    db.connect()
+    db.create_tables([Chambre, Client, Reservation])
+    db.close()
+
     app.register_blueprint(main_bp)
 
     return app
